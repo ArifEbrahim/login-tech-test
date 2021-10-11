e54df051c772f8241fe92ed9d290c44459cbcf95
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-10-11 08:57:14 +0100

    Login and Policy routes setup and working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Policy from './Policy/Policy'
+import Login from './Login/Login'
 
 function App() {
   return (
-    <div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route exact path='/policy' component={Policy} />
+      </Switch>
+    </Router>
 
-    </div>
   );
 }
 
