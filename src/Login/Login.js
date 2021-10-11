1e65b77040ef8012b788be459bf0965f5939c3a3
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-10-11 09:47:48 +0100

    added username and password fields

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Login() {
   return (
+    <>
     <h2 data-testid='login-header-text'>Sign In</h2>
+    <label htmlFor='username'>Username:</label>
+    <input type='text' id='username'></input>
+    <label htmlFor='password'>Password:</label>
+    <input type='text' id='password'></input>
+    </>
   )
 }
--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Login from "./Login";
-import { cleanup, render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import { cleanup, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-afterEach(cleanup)
+afterEach(cleanup);
 
-it('should have a header', () => {
-  render(<Login />)
-  const headerEl = screen.getByTestId('login-header-text')
+it("should have a header", () => {
+  render(<Login />);
+  const headerEl = screen.getByTestId("login-header-text");
   expect(headerEl).toBeInTheDocument();
-  expect(headerEl.innerHTML).toBe('Sign In')
-})
+  expect(headerEl.innerHTML).toBe("Sign In");
+});
+
+it("should have a username and password field", () => {
+  render(<Login />);
+  const usernameField = screen.getByLabelText(/username/i)
+  const passwordField = screen.getByLabelText(/password/i)
+  expect(usernameField).toBeInTheDocument()
+  expect(passwordField).toBeInTheDocument()
+});
