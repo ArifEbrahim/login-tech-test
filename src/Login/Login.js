0315279b8c35fa47de552e2fd8508271171446fe
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-10-11 09:56:29 +0100

    added call to axios on click event

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import axios from 'axios'
 
 export default function Login() {
+
+  const handleClick = () => {
+    axios.post()
+  }
+
   return (
     <>
     <h2 data-testid='login-header-text'>Sign In</h2>
@@ -8,7 +14,7 @@ export default function Login() {
     <input type='text' id='username'></input>
     <label htmlFor='password'>Password:</label>
     <input type='text' id='password'></input>
-    <button data-testid='submit-btn'></button>
+    <button data-testid='submit-btn' onClick={handleClick}>Sign in</button>
     </>
   )
 }
--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -2,6 +2,10 @@ import React from "react";
 import Login from "./Login";
 import { cleanup, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import axios from 'axios';
+
+jest.mock('axios')
 
 afterEach(cleanup);
 
@@ -24,4 +28,11 @@ it('should have a sign in button', () => {
   render(<Login />);
   const submitBtn = screen.getByTestId('submit-btn')
   expect(submitBtn).toBeInTheDocument()
+})
+
+it('should call axios on submit', ()=> {
+  render(<Login />);
+  const submitBtn = screen.getByTestId('submit-btn');
+  userEvent.click(submitBtn);
+  expect(axios.post).toHaveBeenCalled()
 })
