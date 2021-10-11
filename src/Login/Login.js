c4385f155ca485292ac17f5765f1b8233ed96f66
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-10-11 10:22:12 +0100

    API access token now stored locally

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,34 +1,47 @@
-import React, {useState} from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const url = 'https://api.bybits.co.uk/auth/token'
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const url = "https://api.bybits.co.uk/auth/token";
   const config = {
-    'headers': {
-      'environment': 'mock',
-      'Content-Type': 'application/json'
-    }
-  }
-  
-  const handleClick = () => {
+    headers: {
+      environment: "mock",
+      "Content-Type": "application/json",
+    },
+  };
+
+  const handleClick = async () => {
     const data = {
       username: username,
       password: password,
-      "type":"USER_PASSWORD_AUTH"
+      type: "USER_PASSWORD_AUTH",
+    };
+    const response = await axios.post(url, data, config);
+    if (response) {
+      localStorage.setItem("token", response.data.access_token);
     }
-    axios.post(url, data, config)
-  }
+  };
 
   return (
     <>
-    <h2 data-testid='login-header-text'>Sign In</h2>
-    <label htmlFor='username'>Username:</label>
-    <input type='text' id='username' onChange={(e)=>setUsername(e.target.value)}></input>
-    <label htmlFor='password'>Password:</label>
-    <input type='text' id='password' onChange={(e)=>setPassword(e.target.value)}></input>
-    <button data-testid='submit-btn' onClick={handleClick}>Sign in</button>
+      <h2 data-testid="login-header-text">Sign In</h2>
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        onChange={e => setUsername(e.target.value)}
+      ></input>
+      <label htmlFor="password">Password:</label>
+      <input
+        type="text"
+        id="password"
+        onChange={e => setPassword(e.target.value)}
+      ></input>
+      <button data-testid="submit-btn" onClick={handleClick}>
+        Sign in
+      </button>
     </>
-  )
+  );
 }
--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Login from "./Login";
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
-import axios from 'axios';
+import axios from "axios";
 
-jest.mock('axios')
+jest.mock("axios");
 
 afterEach(cleanup);
 
@@ -18,47 +18,59 @@ it("should have a header", () => {
 
 it("should have a username and password field", () => {
   render(<Login />);
-  const usernameField = screen.getByLabelText(/username/i)
-  const passwordField = screen.getByLabelText(/password/i)
-  expect(usernameField).toBeInTheDocument()
-  expect(passwordField).toBeInTheDocument()
+  const usernameField = screen.getByLabelText(/username/i);
+  const passwordField = screen.getByLabelText(/password/i);
+  expect(usernameField).toBeInTheDocument();
+  expect(passwordField).toBeInTheDocument();
 });
 
-it('should have a sign in button', () => {
+it("should have a sign in button", () => {
   render(<Login />);
-  const submitBtn = screen.getByTestId('submit-btn')
-  expect(submitBtn).toBeInTheDocument()
-})
+  const submitBtn = screen.getByTestId("submit-btn");
+  expect(submitBtn).toBeInTheDocument();
+});
 
-it('should call axios on submit', ()=> {
+it("should call axios on submit", () => {
   render(<Login />);
-  const submitBtn = screen.getByTestId('submit-btn');
+  const submitBtn = screen.getByTestId("submit-btn");
   userEvent.click(submitBtn);
-  expect(axios.post).toHaveBeenCalled()
-})
+  expect(axios.post).toHaveBeenCalled();
+});
 
-it('should submit the correct data to the API', () => {
-  axios.post.mockReturnValue( {data: {
-    access_token:''
-  }})
+it("should submit the correct data to the API", () => {
   render(<Login />);
-  const url = 'https://api.bybits.co.uk/auth/token'
+  const url = "https://api.bybits.co.uk/auth/token";
   const config = {
-    'headers': {
-      'environment': 'mock',
-      'Content-Type': 'application/json'
-    }
-  }
+    headers: {
+      environment: "mock",
+      "Content-Type": "application/json",
+    },
+  };
   const data = {
-    "username":"testuser",
-    "password":"1234",
-    "type":"USER_PASSWORD_AUTH"
-  }
-  const usernameField = screen.getByLabelText(/username/i)
-  const passwordField = screen.getByLabelText(/password/i)
-  const submitBtn = screen.getByTestId('submit-btn')
-  userEvent.type(usernameField, data.username)
-  userEvent.type(passwordField, data.password)
-  userEvent.click(submitBtn)
-  expect(axios.post).toHaveBeenCalledWith(url, data, config)
-})
+    username: "testuser",
+    password: "1234",
+    type: "USER_PASSWORD_AUTH",
+  };
+  const usernameField = screen.getByLabelText(/username/i);
+  const passwordField = screen.getByLabelText(/password/i);
+  const submitBtn = screen.getByTestId("submit-btn");
+  userEvent.type(usernameField, data.username);
+  userEvent.type(passwordField, data.password);
+  userEvent.click(submitBtn);
+  expect(axios.post).toHaveBeenCalledWith(url, data, config);
+});
+
+it("should store the access token returned by the API", async () => {
+  axios.post.mockReturnValue({
+    data: {
+      access_token: "123",
+    },
+  });
+  render(<Login />);
+  const submitBtn = screen.getByTestId("submit-btn");
+  userEvent.click(submitBtn);
+  await waitFor(() => {
+    expect(axios.post).toHaveBeenCalled();
+  });
+  expect(localStorage.getItem("token")).toBe("123");
+});
