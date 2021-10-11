70b0698535f27d143029291c3bc148676d0a63e5
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-10-11 09:42:36 +0100

    refactored login header

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div data-testid='login-header-text'>Sign In</div>
+    <h2 data-testid='login-header-text'>Sign In</h2>
   )
 }
